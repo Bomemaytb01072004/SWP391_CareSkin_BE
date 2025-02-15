8989d343b809f34492d338b5e3b72b3c04b4cf4d
vinh <poligun0312@gmail.com>
2025-02-15 18:19:57 +0700

    git fetch origin

--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
+++ SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e7f11a0981d51262ed12ca7928e989fa8648c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5fa1d1d0ef8c08e4f411526d2e8c5258c2773b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
