243a7366d03e67140218dd624fa46f799b1e9353
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-02-18 15:17:18 +0700

    fix: fix conflict after merge

--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
+++ SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a201244ec55cafc11b7b7abafabb1f517d1d59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8126ef3bcc2a901f98ab533cfa83f7e096f0d1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
