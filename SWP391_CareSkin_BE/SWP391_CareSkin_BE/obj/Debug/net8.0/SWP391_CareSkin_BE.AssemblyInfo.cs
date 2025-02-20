5956fc37e3e36c78aa5064a524fa78152116ec19
vinh <poligun0312@gmail.com>
2025-02-20 15:13:43 +0700

    update hasdpassword register

--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
+++ SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9824efb6b0adbdaba07dc10b8ebf5a6e39b4d25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204d6f6d411380881b4c9341769b2ce88da97dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
