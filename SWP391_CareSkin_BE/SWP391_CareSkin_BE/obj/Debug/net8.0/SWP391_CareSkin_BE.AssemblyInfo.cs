6ccc9be28117002dbf417f807698ed7a45353161
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-02-21 00:33:03 +0700

    resolve before commit

--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
+++ SWP391_CareSkin_BE/SWP391_CareSkin_BE/obj/Debug/net8.0/SWP391_CareSkin_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845c8ec9fcb4c8756e59e4a982205b30fc438d11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d5e6bc877116a2d776fc9aec9272bd13f76ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_CareSkin_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
