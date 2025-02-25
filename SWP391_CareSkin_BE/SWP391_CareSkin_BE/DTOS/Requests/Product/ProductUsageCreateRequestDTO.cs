8a69ff6bb6894297597ed541f4ba5309c476669c
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-02-25 23:12:07 +0700

    fix: delete duplicate folder

--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/DTOS/Requests/Product/ProductUsageCreateRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SWP391_CareSkin_BE.DTOS.Requests
-{
-    public class ProductUsageCreateRequestDTO
-    {
-        public int Step { get; set; }
-        public string Instruction { get; set; }
-    }
-}
--- SWP391_CareSkin_BE/SWP391_CareSkin_BE/Services/Interfaces/IFirebaseService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SWP391_CareSkin_BE.Services.Interfaces
-{
-    public interface IFirebaseService
-    {
-        Task<string> UploadImageAsync(Stream fileStream, string fileName);
-        Task<bool> DeleteImageAsync(string fileName);
-    }
-}
