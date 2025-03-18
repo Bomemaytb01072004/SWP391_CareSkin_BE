00cb8696ae9cdb4aff35ef352fa00054530644d7
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-03-18 23:04:50 +0700

    fix: update brand

--- SWP391_CareSkin_BE/DTOS/Requests/Brand/BrandUpdateRequestDTO.cs
+++ SWP391_CareSkin_BE/DTOS/Requests/Brand/BrandUpdateRequestDTO.cs
@@ -5,6 +5,6 @@ namespace SWP391_CareSkin_BE.DTOS.Requests
     public class BrandUpdateRequestDTO
     {
         public string Name { get; set; }
-        public IFormFile PictureFile { get; set; }
+        public IFormFile? PictureFile { get; set; }
     }
 }
