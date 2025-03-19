75f88c9db69c43a54a57399daf9e155eecd1aba3
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-03-20 02:55:52 +0700

    fix migration

--- SWP391_CareSkin_BE/Migrations/20250319194052_UpdateDbv51.cs
+++ SWP391_CareSkin_BE/Migrations/20250319194052_UpdateDbv51.cs
@@ -10,99 +10,13 @@ namespace SWP391_CareSkin_BE.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_BlogNew_Admin_AdminId",
-                table: "BlogNew");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_BlogNew_Staff_StaffId",
-                table: "BlogNew");
-
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_BlogNew",
-                table: "BlogNew");
-
-            migrationBuilder.RenameTable(
-                name: "BlogNew",
-                newName: "BlogNew");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_BlogNew_StaffId",
-                table: "BlogNew",
-                newName: "IX_BlogNews_StaffId");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_BlogNew_AdminId",
-                table: "BlogNew",
-                newName: "IX_BlogNews_AdminId");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_BlogNew",
-                table: "BlogNew",
-                column: "BlogId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_BlogNews_Admin_AdminId",
-                table: "BlogNew",
-                column: "AdminId",
-                principalTable: "Admin",
-                principalColumn: "AdminId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_BlogNews_Staff_StaffId",
-                table: "BlogNew",
-                column: "StaffId",
-                principalTable: "Staff",
-                principalColumn: "StaffId");
+            
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_BlogNews_Admin_AdminId",
-                table: "BlogNew");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_BlogNews_Staff_StaffId",
-                table: "BlogNew");
-
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_BlogNews",
-                table: "BlogNew");
-
-            migrationBuilder.RenameTable(
-                name: "BlogNew",
-                newName: "BlogNew");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_BlogNews_StaffId",
-                table: "BlogNew",
-                newName: "IX_BlogNew_StaffId");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_BlogNews_AdminId",
-                table: "BlogNew",
-                newName: "IX_BlogNew_AdminId");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_BlogNew",
-                table: "BlogNew",
-                column: "BlogId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_BlogNew_Admin_AdminId",
-                table: "BlogNew",
-                column: "AdminId",
-                principalTable: "Admin",
-                principalColumn: "AdminId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_BlogNew_Staff_StaffId",
-                table: "BlogNew",
-                column: "StaffId",
-                principalTable: "Staff",
-                principalColumn: "StaffId");
+            
         }
     }
 }
